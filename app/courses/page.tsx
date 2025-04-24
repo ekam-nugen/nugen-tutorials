afe8e726a6bb843177bee9c0a232e941bfe792a7
Harsimran0429 <156643361+Harsimran0429@users.noreply.github.com>
2025-04-24 16:25:42 +0530

    feat: course description component

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -2,12 +2,14 @@ import CourseTargets from "@/src/components/courses/courseTargets";
 import EnrolmentCard from "@/src/components/courses/EnrolmentCard";
 import FeaturesSection from "@/src/components/courses/featuresSection";
 import CourseBanner from "@/src/components/courses/herosection";
+import HowItWorkSection from "@/src/components/courses/HowItWorksSection";
 import CareerOpportunities from "@/src/components/courses/opportunities";
 
 export default function Home() {
     return (
         <>
             <CourseBanner />
+            <HowItWorkSection />
             <FeaturesSection />
             <CourseTargets />
             <CareerOpportunities />
