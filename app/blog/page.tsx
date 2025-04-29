8454f66e863475a84d95cb9c27788e05f8a8097f
simran kaur <simrannugen@gmail.com>
2025-04-29 12:30:53 +0530

    feat: add loader in pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,29 @@
-
-import BlogSectionPage from '@/src/components/blog/blogsection'
-import React from 'react'
+"use client";
+import BlogSectionPage from "@/src/components/blog/blogsection";
+import Loader from "@/src/components/common/Loader";
+import React, { useEffect, useState } from "react";
 
 function BlogPage() {
-    return (
-        <BlogSectionPage />
-    )
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <BlogSectionPage />
+        </>
+      )}
+    </>
+  );
 }
 
-export default BlogPage
+export default BlogPage;
--- app/contactUs/page.tsx
+++ app/contactUs/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+import Loader from "@/src/components/common/Loader";
 import ContactUs from "@/src/components/contactUs";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <ContactUs />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <ContactUs />
+        </>
+      )}
     </>
   );
 }
