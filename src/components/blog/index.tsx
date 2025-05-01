52d56ceb04d26b7df35c8ed6a68fca22a27eadf4
simran kaur <simrannugen@gmail.com>
2025-05-01 12:19:08 +0530

    refactor:refactor

--- src/components/blog/index.tsx
+++ src/components/blog/index.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import Services from "../about/Services";
 import TeamChoose from "../OurTeam/teamChoose";
-import AwardsSection from "./awardAection";
 import BlogSectionPage from "./blogsection";
 
 export default function BlogPageSection() {
   return (
     <>
       <BlogSectionPage />
-      <Services />
-      <AwardsSection />
       <TeamChoose />
     </>
   );
