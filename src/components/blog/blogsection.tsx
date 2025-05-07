01afeed3e75e6da005e7908a4c937df99df39b32
simran kaur <simrannugen@gmail.com>
2025-05-07 17:17:57 +0530

    fix : text color

--- src/components/blog/blogsection.tsx
+++ src/components/blog/blogsection.tsx
@@ -20,7 +20,7 @@ const BlogSection = () => {
       : blogData.filter((blog) => blog.categorie === selectedCategory);
 
   return (
-    <section className="bg-white py-16 px-4 md:px-20">
+    <section className="bg-white py-16 text-black px-4 md:px-20">
       <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-8">
         {/* Blog List */}
         <div className="lg:col-span-2 space-y-14">
